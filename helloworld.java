dc1086f4be7f24cb10552613bb347972ae34d7f9
Sarvesh B Gugale <144622623+SarveshGugale@users.noreply.github.com>
2025-01-03 08:55:18 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
-class simple{
-
+class simple {
     public static void main(String args[]){
         System.out.println("Hello World");
+        System.out.println("Sarvesh madhe some changes first time");        
     }
 }
